2dc7c34f6a3a553d4c4f344e101b534e30f967cb
Fernando Binasco <fernando.binasco@gmail.com>
2021-07-23 15:23:06 -0300

    correcao do Firebase.initializeApp()

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
-import 'package:chat_online/chat_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:chat_online/chat_screen.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -12,7 +15,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
         iconTheme: IconThemeData(
           color: Colors.blue
